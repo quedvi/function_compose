23d043a78f63bd12335b67e9d3c651972162fa00
Oehzelt Martin <oehzelt@pixelpoint.at>
2021-02-09 19:21:32 +0100

    improve formatting

--- tools/function.py
+++ tools/function.py
@@ -1,13 +1,10 @@
 from .function_inner import FunctionInner
 class Function:
-    
     def __init__(self, func):
         self.func = func
         
     def __call__(self, *args, **kwargs):
-        
         @FunctionInner
         def inner(x):
-            return self.func(x, *args, **kwargs)
-            
+            return self.func(x, *args, **kwargs)         
         return inner
